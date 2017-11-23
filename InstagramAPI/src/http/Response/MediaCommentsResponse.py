9a682f4ca526b718fbf3c8b231e8ae7ee878088b
Kevin <kevpgoff@users.noreply.github.com>
2017-11-23 14:59:12 -0500

    Update MediaCommentsResponse.py

--- InstagramAPI/src/http/Response/MediaCommentsResponse.py
+++ InstagramAPI/src/http/Response/MediaCommentsResponse.py
@@ -9,6 +9,21 @@ class MediaCommentsResponse(Response):
         if self.STATUS_OK == response['status']:
             if 'media' in response and response['media']:
                 self.item = Item(response['media'])
+
+            if not response.get('comments_disabled', False):
+
+                self.comments = response['comments']
+
+                if response['has_more_comments']:
+                    self.has_more = True
+                    self.next_max_id = response['next_max_id']
+                else:
+                    self.has_more = False
+
+            else:
+                self.comments = []
+                self.has_more = False
+
         else:
             self.setMessage(response['message'])
         self.setStatus(response['status'])
