ffb724dbe4a5170462c3760af91a34cb0ca8b459
Kevin <kevpgoff@users.noreply.github.com>
2017-11-23 23:13:22 -0500

    unicode pls 2

--- InstagramAPI/src/http/HttpInterface.py
+++ InstagramAPI/src/http/HttpInterface.py
@@ -485,7 +485,7 @@ class HttpInterface(object):
             if 'headers' in b and isinstance(b['headers'], list):
                 for header in b['headers']:
                     body += ("\r\n" + header)
-            body += ("\r\n\r\n" + b['data'].encode('utf-8').decode('utf-8) + "\r\n")
+            body += ("\r\n\r\n" + b['data'].encode('utf-8').decode('utf-8') + "\r\n")
         body += ('--' + boundary + '--')
 
         return body
